954b3fe691951d8f6bbf4a6c25eb2945a64c85d9
Andy Cruz <e5ce6c2@gmail.com>
2018-06-14 01:33:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -44,8 +44,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array)
 
 function removeElementFromEndOfArray(array)
 {
-  return array.splice(-1, 1)
-
+  return array.slice(0, array.length() - 1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array)
