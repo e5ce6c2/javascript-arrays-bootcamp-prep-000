8a384b9f507e4c819358924dc82aa3e67258a764
Andy Cruz <e5ce6c2@gmail.com>
2018-06-14 01:15:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,36 @@ function destructivelyAddElementToBeginningOfArray(array, element)
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element)
+{
+  var Array = [...array, element]
+  return Array
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  return array.pop(element)
+}
+
+function accessElementInArray(array, index)
+{
+  return array[index]
+}
+
+function removeElementFromBeginningOfArray(array)
+{
+  var Array = array.splice(1)
+  return Array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array)
+{
+  return array.splice(1)
+}
+
+function removeElementFromEndOfArray(array)
+{
+  var Array = array.splice()
+  return Array.splice
+}
